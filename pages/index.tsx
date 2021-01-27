7c32edbf11e55056e869fadc2d757beac1422efc
ramanverma2k <ramanverma4183@gmail.com>
2021-01-27 12:50:52 +0530

    components: add custom fonts and icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,7 @@
+import Landing from "./landing";
+
 export default function Home() {
-    return <div className='flex justify-center items-center h-screen font-bold'>Hi from Next.js App with TailwindCSS and Typescript</div>
+    return (
+        <Landing />
+    )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      primary: '#011627',
+      secondary: '#fdfffc',
+      ob1: '#e71d36',
+      ob2: '#2ec4b6',
+    },
+    fontFamily: {
+      quicksand: ['Quicksand'],
+      right: ['Rightism']
+    }
   },
   variants: {
     extend: {},
