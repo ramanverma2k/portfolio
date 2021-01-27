3d83374147274f5d9b1528fb3e2513b685a958e5
ramanverma2k <ramanverma4183@gmail.com>
2021-01-27 23:45:06 +0530

    create about,blog and projects page

---
+++ pages/about.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+    return(
+        <div className="flex justify-center mt-40">
+            <p className="neon-outline text-4xl text-primary subpixel-antialiased font-quicksand font-bold">
+                About
+            </p>
+        </div>
+    )
+}
---
+++ pages/blog.tsx
@@ -0,0 +1,9 @@
+export default function Blog() {
+    return (
+        <div className="flex justify-center mt-40">
+            <p className="neon-outline text-4xl text-primary subpixel-antialiased font-quicksand font-bold">
+                Blog
+            </p>
+        </div>
+    )
+}
