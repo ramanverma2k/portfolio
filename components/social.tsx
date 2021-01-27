501d59df937901cea799f728066e79f1feefed3f
ramanverma2k <ramanverma4183@gmail.com>
2021-01-27 12:54:48 +0530

    add component to access social profiles

---
+++ components/social.tsx
@@ -0,0 +1,23 @@
+import Github from '../components/icons/github.svg'
+import LinkedIn from '../components/icons/linkedin.svg'
+import Telegram from '../components/icons/telegram.svg'
+import Mail from '../components/icons/mail.svg'
+
+export default function SocialLinks() {
+    return (
+        <div className="text-secondary flex flex-wrap justify-center space-x-4">
+                <a className="pt-1" href="https://www.github.com/ramanverma2k">
+                    <Github class="fill-current text-ob2"/>
+                </a>
+                <a href="https://www.linkedin.com/in/ramanverma2k">
+                    <LinkedIn class="fill-current text-ob2"/>
+                </a>
+                <a className="pt-1" href="@nottaggingraman">
+                    <Telegram class="fill-current text-ob2"/>
+                </a>
+                <a className="pt-1" href="mailto:ramanverma4183@gmail.com">
+                    <Mail class="fill-current text-ob2"/>
+                </a>
+        </div>
+    )
+}
